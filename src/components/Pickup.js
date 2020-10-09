7c5254cfce67655c3a3f548f601c9dd5298489df
github username <github email address>
2020-10-09 12:32:57 -0700

    two sections in home screen

---
+++ src/components/Pickup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+
+export default function Pickup() {
+  return (
+    <div className="Main">
+      <h3>Pick Up Now</h3>
+      <img src="cinna.jpg" alt="Cinna" className="image" />
+    </div>
+  );
+}
