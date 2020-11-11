1bef30e97147e48cd50db4a66a57d4fc762abdd4
github username <github email address>
2020-11-11 12:45:45 -0800

    now has order screens

---
+++ src/components/AllItems.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AllItems() {
+  return (
+    <div>
+      <h1>All Available Items</h1>
+    </div>
+  );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,23 @@ import React from "react";
 import "../App.css";
 import Main from "./Main.js";
 import Pickup from "./Pickup.js";
+import HotItems from "./HotItems.js";
+import AllItems from "./AllItems.js";
 
 export default function Home() {
+  const [page, setPage] = React.useState(0);
   return (
-    <div className="maindiv">
-      <Main />
-      <Pickup />
+    <div>
+      {page === 0 ? (
+        <div className="maindiv">
+          <Main setPage={setPage} />
+          <Pickup setPage={setPage} />
+        </div>
+      ) : page === 1 ? (
+        <HotItems />
+      ) : (
+        <AllItems />
+      )}
     </div>
   );
 }
